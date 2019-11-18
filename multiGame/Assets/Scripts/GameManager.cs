c0b61a9978c00cb3a38354e8852fb969a5089e3b
akdagYazilim <prodembilgiislem@gmail.com>
2019-11-18 16:34:24 +0300

    Denemeler

--- multiGame/Assets/Scripts/GameManager.cs
+++ multiGame/Assets/Scripts/GameManager.cs
@@ -11,13 +11,10 @@ using Photon.Realtime;
 public class GameManager : MonoBehaviourPunCallbacks
 {
     public GameObject _gameCharacterPreFab;
-    [Header("Game UI Buttons")]
-    public GameObject _logOutPanel;
-    public GameObject _tabPanel;
-    public Text _oyuncuSayisi;
+    public Text roomName;
     void Start()
     {
-        _oyuncuSayisi.text = "Oyuncu Sayısı : " + PhotonNetwork.CurrentRoom.PlayerCount;
+        
 
         float x = Random.Range(-4f, 4f);
         float z = Random.Range(-4f, 4f);
@@ -25,54 +22,20 @@ public class GameManager : MonoBehaviourPunCallbacks
 
         if (PhotonNetwork.IsConnectedAndReady)
         {
+            roomName.text = "Oda No : " + PhotonNetwork.CurrentRoom.Name;
             PhotonNetwork.Instantiate(_gameCharacterPreFab.name, cikisYeri, Quaternion.identity);
         }
     }
     
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (_logOutPanel.activeSelf == true)
-            {
-                _logOutPanel.SetActive(false);
-            }
-            else
-            {
-                _logOutPanel.SetActive(true);
-            }
-        }
 
-        KeyTab();
 
     }
 
-    public void logOut()
-    {
-        PhotonNetwork.LeaveRoom();
-        SceneManager.LoadScene("LobbyScene");
-    }
+ 
 
-    public void KeyTab()
-    {
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            _tabPanel.SetActive(true);
-        }
 
-        if (Input.GetKeyUp(KeyCode.Tab))
-        {
-            _tabPanel.SetActive(false);
-        }
-    }
 
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        _oyuncuSayisi.text = "Oyuncu Sayısı : " + PhotonNetwork.CurrentRoom.PlayerCount;
-    }
 
-    public override void OnPlayerLeftRoom(Player otherPlayer)
-    {
-        _oyuncuSayisi.text = "Oyuncu Sayısı : " + PhotonNetwork.CurrentRoom.PlayerCount;
-    }
 }
--- multiGame/Assets/Scripts/LaunchManager.cs
+++ multiGame/Assets/Scripts/LaunchManager.cs
@@ -6,31 +6,14 @@ using Photon.Pun;
 using Photon.Realtime;
 public class LaunchManager : MonoBehaviourPunCallbacks
 {
-    [Header("Input Fields")]
-    public InputField _nameInputField;
-    public InputField _odaIsmiFiled;
-    public InputField _oyuncuSayisi;
 
-    [Header("Login Panel")]
-    public GameObject _loginPanel;
-    public GameObject[] _loginPanelGameObject;
-    [Header("Game Settings Panel")]
-    public GameObject _gameSettingsPanel;
-    public GameObject[] _gameSettingsPanelGameObject;
-    [Header("Create Join Room Panel")]
-    public GameObject _createRoomPanel;
-    public GameObject[] _createRoomPanelGameObject;
-    [Header("Random Room Panel")]
-    public GameObject _randomRoomPanel;
-    public GameObject[] _randomRoomPanelGameObject;
-
-    public Text randomText;
     void Start()
     {
-        ActiveLoginPanel();
         PhotonNetwork.AutomaticallySyncScene = true;
-        //onGiris();
-        //PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.ConnectUsingSettings();
+
+        //ActiveLoginPanel();
+        //PhotonNetwork.AutomaticallySyncScene = true;        
     }
 
     // Update is called once per frame
@@ -42,9 +25,12 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     #region PhotonCallBack
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Photona Bağlanıldı.");
-        Debug.Log(PhotonNetwork.NickName);
-        ActiveGameSettingPanel();
+        Debug.Log("Photon'a bağlandınız.");
+        OdaOlustur();
+
+        //Debug.Log("Photona Bağlanıldı.");
+        //Debug.Log(PhotonNetwork.NickName);
+        //ActiveGameSettingPanel();
     }
     public override void OnConnected()
     {
@@ -52,100 +38,25 @@ public class LaunchManager : MonoBehaviourPunCallbacks
     }
     #endregion
 
-    #region Unity Methods
-    public void ActiveLoginPanel()
-    {
-        _loginPanel.SetActive(true);
-        foreach (GameObject gameObject in _loginPanelGameObject)
-        {
-            gameObject.SetActive(true);
-        }
-    }
-
-    public void ActiveRandomRoomPanel()
-    {
-        _gameSettingsPanel.SetActive(false);
-        foreach (GameObject gameObject in _gameSettingsPanelGameObject)
-        {
-            gameObject.SetActive(false);
-        }
-
-        _randomRoomPanel.SetActive(true);
-        foreach (GameObject gameObject in _randomRoomPanelGameObject)
-        {
-            gameObject.SetActive(true);
-        }
-        PhotonNetwork.JoinRandomRoom();//Boş oda bulana kadar arıyor.
-        randomText.text = "Bir oda aranıyor";
-    }
-
     public override void OnJoinedRoom() // Odaya girince yani oyuna girince
     {
-        Debug.Log(PhotonNetwork.CurrentRoom.Name);
         PhotonNetwork.LoadLevel("GameScene");
     }
 
-    public override void OnJoinRandomFailed(short returnCode, string message) //Oda bulmassa kendisi kurmak için.
+    public override void OnCreatedRoom() // Oda oluştuktan sonra
     {
-        Debug.Log("Oda bulunamadı ben kuruyorum.");
-        odaKur();
+        
     }
 
-    public void odaKur()
+    public void OdaOlustur()
     {
-        string randomIsım = "Room "+ Random.Range(10, 100);
+        //string randomIsım = "Room " + Random.Range(10, 100);
+        string randomIsım = "Room 96";
         RoomOptions odaAyar = new RoomOptions();
         odaAyar.MaxPlayers = 20;
         odaAyar.IsOpen = true;
         odaAyar.IsVisible = true;
         PhotonNetwork.CreateRoom(randomIsım, odaAyar);
     }
-
-    public void ActiveGameSettingPanel()
-    {
-        _loginPanel.SetActive(false);
-        foreach (GameObject gameObject in _loginPanelGameObject)
-        {
-            gameObject.SetActive(false);
-        }
-
-        _gameSettingsPanel.SetActive(true);
-        foreach (GameObject gameObject in _gameSettingsPanelGameObject)
-        {
-            gameObject.SetActive(true);
-        }
-    }
-    public void onGiris()
-    {
-        Debug.Log("Button Çalıştı.");
-        PhotonNetwork.NickName = _nameInputField.text;
-        PhotonNetwork.ConnectUsingSettings();
-    }
-
-    public void odaOlusturPaneleGit()
-    {
-        ActiveCreateJoinRoomPanel();
-    }
-
-    public void ActiveCreateJoinRoomPanel()
-    {
-        _gameSettingsPanel.SetActive(false);
-        foreach (GameObject gameObject in _gameSettingsPanelGameObject)
-        {
-            gameObject.SetActive(false);
-        }
-        _createRoomPanel.SetActive(true);
-        foreach (GameObject gameObject in _createRoomPanelGameObject)
-        {
-            gameObject.SetActive(true);
-        } 
-    }
-
-    public void odaOlustur()
-    {
-        RoomOptions roomCount = new RoomOptions();
-        roomCount.MaxPlayers = (byte)int.Parse(_oyuncuSayisi.text);
-        PhotonNetwork.CreateRoom(_odaIsmiFiled.text, roomCount);
-    }
-    #endregion
+   
 }
